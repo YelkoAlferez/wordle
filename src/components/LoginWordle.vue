d427862d12a25c67901f5b9b57f9fe946a45ff5f
YelkoAlferez <alferezyelko@gmail.com>
2024-10-17 19:19:29 +0200

    changes for app

---
+++ src/components/LoginWordle.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+</script>
+<template>
+  <form action="" class="form-control">
+    <div class="form-group mb-2">
+      <label for="email">Email</label>
+      <input
+        type="text"
+        placeholder="example@test.com"
+        name="email"
+        id="email"
+      />
+    </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" />
+    </div>
+  </form>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
